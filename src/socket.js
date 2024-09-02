84a0c4b0eb22ef9e97e867335b9dbdb60c1e317b
Saumya Darne <samd1904@gmail.com>
2024-09-03 01:00:47 +0530

    Base chat app working done

---
+++ src/socket.js
@@ -0,0 +1,34 @@
+import { reactive } from "vue";
+import { io } from "socket.io-client";
+
+export const state = reactive({
+  connected: false,
+  fooEvents: [],
+  barEvents: [],
+  chatEvents: []
+});
+
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = "http://localhost:3000/";
+
+export const socket = io(URL);
+
+socket.on("connect", () => {
+  state.connected = true;
+});
+
+socket.on("disconnect", () => {
+  state.connected = false;
+});
+
+socket.on("foo", (...args) => {
+  state.fooEvents.push(args);
+});
+
+socket.on("bar", (...args) => {
+  state.barEvents.push(args);
+});
+
+socket.on("chat message", (...args) => {
+  state.chatEvents.push(args[0]);
+});
